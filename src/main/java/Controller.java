a040eacd4e7f91c1d4d079aa243443913ed92f03
Michał Kalinowski <kalinowski5@gmail.com>
2019-01-27 17:05:51 +0100

    Play with different storage engines

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,7 +2,10 @@ import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.config.Configuration;
 import org.axonframework.config.DefaultConfigurer;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
-import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;  //In memory
+import org.axonframework.eventsourcing.eventstore.jdbc.JdbcEventStorageEngine;          //Basic SQL
+import org.axonframework.eventsourcing.eventstore.jpa.JpaEventStorageEngine;            //ORM
 
 public class Controller {
 
@@ -10,9 +13,11 @@ public class Controller {
     {
         System.out.println("Configuring axon...");
 
+        final EventStorageEngine storageEngine = new InMemoryEventStorageEngine();
+
         Configuration configuration = DefaultConfigurer.defaultConfiguration()
                 .configureAggregate(Game.class)
-                .configureEventStore(c -> new EmbeddedEventStore(new InMemoryEventStorageEngine()))
+                .configureEventStore(c -> new EmbeddedEventStore(storageEngine))
                 //.registerModule(eventHandlingConfiguration)
                // .registerQueryHandler(c -> projection) // (4)
                 .buildConfiguration();
