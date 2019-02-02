ff5ef2aecd335380e34977ee6ab7733a9da5ec03
Michał Kalinowski <kalinowski5@gmail.com>
2019-02-02 16:53:31 +0100

    Add level property to the Space value object

--- src/main/java/k2/valueobject/Space.java
+++ src/main/java/k2/valueobject/Space.java
@@ -5,12 +5,14 @@ import java.util.Set;
 public class Space {
     final Integer costOfEntry;
     final Integer altitude;
+    final Integer level;
     final Set<Space> connectionsUpward;
     final Set<Space> connectionsDownward;
 
-    public Space(Integer costOfEntry, Integer altitude, Set<Space> connectionsUpward, Set<Space> connectionsDownward) {
+    public Space(Integer costOfEntry, Integer altitude, Integer level, Set<Space> connectionsUpward, Set<Space> connectionsDownward) {
         this.costOfEntry = costOfEntry;
         this.altitude = altitude;
+        this.level = level;
         this.connectionsUpward = connectionsUpward;
         this.connectionsDownward = connectionsDownward;
     }
