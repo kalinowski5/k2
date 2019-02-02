f134b525cf3050d67e992b9527a6994b57208101
Michał Kalinowski <kalinowski5@gmail.com>
2019-02-02 19:42:43 +0100

    Borad as a directed graph - POC

--- src/main/java/k2/aggregate/Game.java
+++ src/main/java/k2/aggregate/Game.java
@@ -7,15 +7,20 @@ import k2.exception.*;
 import k2.valueobject.Card;
 import k2.valueobject.GameId;
 import k2.valueobject.PawnColor;
+import k2.valueobject.Space;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.commandhandling.model.AggregateIdentifier;
 import org.axonframework.commandhandling.model.AggregateLifecycle;
 import org.axonframework.commandhandling.model.AggregateMember;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Aggregate
 public class Game {
@@ -25,6 +30,8 @@ public class Game {
 
     @AggregateMember
     private Map<PawnColor, Player> players;
+
+    Graph<Space, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
 
     private boolean gameStarted = false;
 
@@ -88,6 +95,34 @@ public class Game {
     public void on(BoardSetUpEvent event) {
         gameId = event.getId();
         players = new HashMap<>();
+
+        Space spaceBaseCamp = new Space("Basecamp", 5000, 1, 1);
+        Space space1 = new Space("1", 5000, 1, 1);
+        Space space2 = new Space("2", 5000, 1, 1);
+        Space space3 = new Space("3", 5000, 1, 1);
+        Space space4 = new Space("4", 5000, 1, 1);
+        Space space5 = new Space("5", 5000, 1, 1);
+        Space space6 = new Space("6", 5000, 1, 1);
+
+        graph.addVertex(spaceBaseCamp);
+        graph.addVertex(space1);
+        graph.addVertex(space2);
+        graph.addVertex(space3);
+        graph.addVertex(space4);
+        graph.addVertex(space5);
+        graph.addVertex(space6);
+
+        graph.addEdge(spaceBaseCamp, space1);
+        graph.addEdge(space1, space2);
+        graph.addEdge(space2, space3);
+        graph.addEdge(space2, space4);
+        graph.addEdge(space3, space5);
+        graph.addEdge(space4, space5);
+
+        DijkstraShortestPath<Space, DefaultEdge> dijkstraAlg = new DijkstraShortestPath<>(graph);
+        ShortestPathAlgorithm.SingleSourcePaths<Space, DefaultEdge> iPaths = dijkstraAlg.getPaths(spaceBaseCamp);
+        System.out.println(iPaths.getPath(space5) + "\n");
+
     }
 
     @EventSourcingHandler
--- src/main/java/k2/valueobject/Space.java
+++ src/main/java/k2/valueobject/Space.java
@@ -1,19 +1,51 @@
 package k2.valueobject;
 
-import java.util.Set;
+import java.util.Objects;
 
 public class Space {
-    final Integer costOfEntry;
+    final String id;
     final Integer altitude;
     final Integer level;
-    final Set<Space> connectionsUpward;
-    final Set<Space> connectionsDownward;
+    final Integer acclimatizationModifier;
+    final Integer costOfEntry;
 
-    public Space(Integer costOfEntry, Integer altitude, Integer level, Set<Space> connectionsUpward, Set<Space> connectionsDownward) {
-        this.costOfEntry = costOfEntry;
+    public Space(String id, Integer altitude, Integer level, Integer acclimatizationModifier) {
+        this.id = id;
         this.altitude = altitude;
         this.level = level;
-        this.connectionsUpward = connectionsUpward;
-        this.connectionsDownward = connectionsDownward;
+        this.acclimatizationModifier = acclimatizationModifier;
+        this.costOfEntry = 1;
+    }
+
+    public Space(String id, Integer altitude, Integer level, Integer acclimatizationModifier, Integer costOfEntry) {
+        this.id = id;
+        this.altitude = altitude;
+        this.level = level;
+        this.acclimatizationModifier = acclimatizationModifier;
+        this.costOfEntry = costOfEntry;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Space compareTo = (Space) o;
+
+        return Objects.equals(compareTo.getId(), this.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Space " + id;
     }
 }
